e33ff9c99985d6568cccebfbed96db726ed80067
Freddy <freddysae00@gmail.com>
2026-04-29 21:21:21 +0200

    chore: add vitest with happy-dom environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -15,5 +16,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'out',
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: true,
   },
 })
